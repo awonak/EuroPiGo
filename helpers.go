9a181b7b3eb47dfdb25f254e53bfad969f6a1486
Adam Wonak <adam.wonak@gmail.com>
2022-10-05 19:00:24 -0500

    Add a debug helper for observing heap per second.

--- helpers.go
+++ helpers.go
@@ -3,6 +3,7 @@ package europi
 import (
 	"log"
 	"runtime"
+	"time"
 )
 
 // Clamp returns a value that is no lower than "low" and no higher than "high".
@@ -20,4 +21,22 @@ func DebugMemoryUsage() {
 	var mem runtime.MemStats
 	runtime.ReadMemStats(&mem)
 	log.Println("Heap Used: ", mem.HeapInuse, " Free: ", mem.HeapIdle, " Sys:", mem.HeapSys, "\r")
+}
+
+func DebugMemoryUsedPerSecond() {
+	var (
+		heapUsed uint64
+		mem      runtime.MemStats
+	)
+	for {
+		runtime.ReadMemStats(&mem)
+		if mem.HeapInuse < heapUsed {
+			log.Println("GC called\r")
+			log.Println("Heap used per second: ", mem.HeapSys-mem.HeapInuse, " Total Heap Used: ", mem.HeapInuse, "\r")
+		} else {
+			log.Println("Heap used per second: ", mem.HeapInuse-heapUsed, " Total Heap Used: ", mem.HeapInuse, "\r")
+		}
+		heapUsed = mem.HeapInuse
+		time.Sleep(time.Second)
+	}
 }
