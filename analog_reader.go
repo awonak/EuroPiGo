21503963714aa426e2d187a7238fad520894383b
Adam Wonak <adam.wonak@gmail.com>
2022-10-14 08:26:15 -0500

    the underlying hardware for analog input and knob are different enough that they need fully separate implementations of the interface.

--- analog_reader.go
+++ analog_reader.go
@@ -2,6 +2,7 @@ package europi
 
 import (
 	"machine"
+	"math"
 )
 
 const (
@@ -24,45 +25,40 @@ type AnalogReader interface {
 	ReadVoltage() float32
 	Percent() float32
 	Range(steps uint16) uint16
-	read() uint16
 }
 
-type analogReader struct {
+type analogInput struct {
 	machine.ADC
-
 	samples uint16
 }
 
-func newAnalogReader(pin machine.Pin) *analogReader {
+func newAnalogInput(pin machine.Pin) *analogInput {
 	adc := machine.ADC{Pin: pin}
 	adc.Configure(machine.ADCConfig{})
-	return &analogReader{
-		ADC:     adc,
-		samples: defaultSamples,
-	}
+	return &analogInput{ADC: adc, samples: defaultSamples}
 }
 
 // Samples sets the number of reads for an more accurate average read.
-func (a *analogReader) Samples(samples uint16) {
+func (a *analogInput) Samples(samples uint16) {
 	a.samples = samples
 }
 
 // Percent return the percentage of the input's current relative range as a float between 0.0 and 1.0.
-func (a *analogReader) Percent() float32 {
+func (a *analogInput) Percent() float32 {
 	return float32(a.read()) / calibratedMaxAI
 }
 
 // ReadVoltage return the current read voltage between 0.0 and 10.0 volts.
-func (a *analogReader) ReadVoltage() float32 {
+func (a *analogInput) ReadVoltage() float32 {
 	return a.Percent() * MaxVoltage
 }
 
 // Range return a value between 0 and the given steps (not inclusive) based on the range of the analog input.
-func (a *analogReader) Range(steps uint16) uint16 {
+func (a *analogInput) Range(steps uint16) uint16 {
 	return uint16(a.Percent() * float32(steps))
 }
 
-func (a *analogReader) read() uint16 {
+func (a *analogInput) read() uint16 {
 	var sum int
 	for i := 0; i < int(a.samples); i++ {
 		sum += Clamp(int(a.Get())-calibratedMinAI, 0, calibratedMaxAI)
@@ -70,22 +66,41 @@ func (a *analogReader) read() uint16 {
 	return uint16(sum / int(a.samples))
 }
 
-type analogInput struct {
-	AnalogReader
-}
-
-func newAnalogInput(pin machine.Pin) *analogInput {
-	return &analogInput{
-		newAnalogReader(pin),
-	}
-}
-
 type knob struct {
-	AnalogReader
+	machine.ADC
+	samples uint16
 }
 
 func newKnob(pin machine.Pin) *knob {
-	return &knob{
-		newAnalogReader(pin),
+	adc := machine.ADC{Pin: pin}
+	adc.Configure(machine.ADCConfig{})
+	return &knob{ADC: adc, samples: defaultSamples}
+}
+
+// Samples sets the number of reads for an more accurate average read.
+func (k *knob) Samples(samples uint16) {
+	k.samples = samples
+}
+
+// Percent return the percentage of the knob's current relative range as a float between 0.0 and 1.0.
+func (k *knob) Percent() float32 {
+	return 1 - float32(k.read())/math.MaxUint16
+}
+
+// ReadVoltage return the current read voltage between 0.0 and 10.0 volts.
+func (k *knob) ReadVoltage() float32 {
+	return k.Percent() * MaxVoltage
+}
+
+// Range return a value between 0 and the given steps (not inclusive) based on the range of the knob's position.
+func (k *knob) Range(steps uint16) uint16 {
+	return uint16(k.Percent() * float32(steps))
+}
+
+func (k *knob) read() uint16 {
+	var sum int
+	for i := 0; i < int(k.samples); i++ {
+		sum += int(k.Get())
 	}
+	return uint16(sum / int(k.samples))
 }
